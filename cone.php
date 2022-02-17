5162936f8d50bf4f469b4f22b47e3e28c6a397ec
missaelcv <missaelcv0220@gmail.com>
2022-02-16 22:30:55 -0400

    agregando gato

---
+++ cone.php
@@ -0,0 +1,22 @@
+<?php
+ 
+ 
+$link = 'mysql:host=localhost;dbname=yt_colores';
+$usuario = 'root';
+//$pass = 'root';
+
+try {
+    
+   $pdo = new PDO($link, $usuario);
+
+   echo 'Conectado !! <br>';
+
+//     foreach($pdo->query('SELECT * FROM `colores`') as $fila) {
+//        print_r($fila);
+// }
+
+} catch (PDOException $e) {
+    print "Error:" . $e->getMessage() . "<br/>";
+    die();
+}
+?>
