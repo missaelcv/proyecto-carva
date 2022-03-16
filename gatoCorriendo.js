9eb9bdfb1db4f4c7b79db06973e59d0ff7850e8a
missaelcv <missaelcv0220@gmail.com>
2022-03-15 23:06:28 -0400

    gato corriendo actualizado

--- gatoCorriendo.js
+++ gatoCorriendo.js
@@ -1,3 +1,12 @@
+let Estado = {
+    ANTES_DE_EMPEZAR: "Aun no ha iniciado",
+    INICIADO: "Iniciado",
+    PAUSADO: "Pausado",
+    TERMINADO: "Terminado"
+};
+
+let estadoDelJuego = Estado.ANTES_DE_EMPEZAR;
+
 var PropCanvas = {
     intervalo: undefined,
     estaPausado: false,
@@ -9,9 +18,8 @@ let miCanvas = document.getElementById("miCanvas");
 let contexto = miCanvas.getContext("2d");
 let anchura = miCanvas.width;
 let alturaCanvas = miCanvas.height;
-let estaSubiendo = false;
-let estaSaltando = false;
-let maximoASaltar = 700;
+
+let maximoASaltar = 600;
 let cantidadASaltar = 0;
 var pausa = new Audio('audio/pausa.mp3');
 var salto = new Audio('audio/salto.mp3');
@@ -22,6 +30,7 @@ let puntuacionMasAlta = 0;
 let restaTiempo = 0;
 let imagenArbol = document.getElementById("arbol");
 let alturaPiso = 20;
+let variacionHorizontal = 20;
 
 let personajes = {
     gato: ["gato1.png", "gato2.png", "gato3.png", "gato4.png", "gato5.png", "gato6.png", "gato7.png", "gato8.png"],
@@ -29,18 +38,19 @@ let personajes = {
     mario: [ "A.png", "B.png", "C.png", "D.png", "E.png", "F.png", "G.png"]
 };
 
+//rellenar el selector de personajes
 let selectorDePersonaje = document.getElementById("selectorDePersonaje");
-
 for(  personajeActual in personajes) {
     let carpeta = personajeActual;
     let rutaImagen = `imagenes/${ carpeta  }/${ personajes[carpeta][0] }`;
 
     console.log(rutaImagen);
-    let opcion = `<div class="contenedorOpcionPersonaje"><img class="imagenPersonaje" src="${ rutaImagen  }" data-nombre="${personajeActual}">`;
-    //opcion += personajeActual;
+    let opcion = `<div class="contenedorOpcionPersonaje"><img class="imagenPersonaje" src="${ rutaImagen  }" data-nombre="${personajeActual}" title="${personajeActual}">`;
     opcion += "</div>";
     selectorDePersonaje.innerHTML += opcion; 
 }
+
+suscribirImagenesDePersonajesAlManejoDeEventos();
 
 function suscribirImagenesDePersonajesAlManejoDeEventos( ) {
     let  imagenes = selectorDePersonaje.getElementsByClassName("imagenPersonaje");
@@ -60,14 +70,84 @@ function suscribirImagenesDePersonajesAlManejoDeEventos( ) {
     }
 }
 
-suscribirImagenesDePersonajesAlManejoDeEventos();
-
 let personaje = {
-    nombre: "mario",
+    nombre: "gato",
     x: 0,
     y: 0,
     anchura: 0,
     altura: 0,
+    estaSaltando: false,
+    estaSubiendo: false,
+    inicializar() {
+        personaje.y = miCanvas.height - personaje.altura;
+    },
+    actualizar() {
+        if(estadoDelJuego == Estado.ANTES_DE_EMPEZAR) {
+            let variacion = 10;
+            personaje.y += personaje.estaSubiendo ? -variacion : variacion;
+            
+            if( personaje.y > miCanvas.height - personaje.altura) {
+                personaje.estaSubiendo = true;
+            }
+            
+            if( personaje.y < miCanvas.height - personaje.altura * 1.5 ) {
+                personaje.estaSubiendo = false;
+            }
+        }
+        else if( estadoDelJuego == Estado.INICIADO ) {
+            if (personaje.estaSaltando) {
+                cantidadASaltar += (personaje.estaSubiendo ? 60 : -60);
+        
+                if (cantidadASaltar > maximoASaltar) {
+                    cantidadASaltar = maximoASaltar;
+                    personaje.estaSubiendo = false;
+                }
+        
+                if (personaje.estaSubiendo) {
+                    personaje.y -= 20;
+        
+                } else {
+        
+                    personaje.y += 20;
+                    if (personaje.y > alturaCanvas - personaje.altura) {
+                        personaje.y = alturaCanvas - personaje.altura;
+                    }
+                    if (cantidadASaltar <= personaje.altura) {
+                        personaje.estaSaltando = false;
+                        cantidadASaltar = 0;
+                    }
+                }
+        
+            }
+
+
+            let arregloPersonajeActual = personajes[ personaje.nombre ];
+
+            if (personaje.estaSaltando) {
+                imagenActual = personaje.estaSubiendo ? arregloPersonajeActual.length - 2 : arregloPersonajeActual.length - 1;
+            }
+            else {
+                imagenActual += 1;
+                if (imagenActual >= personajes[ personaje.nombre ].length) {
+                    imagenActual = 0;
+                }
+            }
+        }
+
+        personaje.imagen = arregloImagenes[imagenActual];
+    },
+    dibujar() {
+        contexto.drawImage(personaje.imagen, personaje.x, personaje.y, personaje.anchura, personaje.altura);
+        contexto.strokeStyle = "green";
+        contexto.strokeRect(personaje.x, personaje.y, personaje.anchura, personaje.altura);
+
+        let hitBox = personaje.hitBox();
+        if ( hitBox ) {
+            contexto.strokeStyle = "red";
+            contexto.strokeRect(hitBox.x, hitBox.y, hitBox.anchura, hitBox.altura);
+
+        }
+    },
     chocaCon(obstaculo) {
         let hitBox = personaje.hitBox();
         if( hitBox ) {
@@ -104,11 +184,38 @@ let obstaculos = [
 ]; //{ x, y, anchura, altura }
 let tamanosArboles = [];
 
-let totalDeObstaculos = 20;
+let totalDeObstaculos = 2;
+
+function dibujarObstaculos() {
+    console.log(obstaculos);
+    for (let i = 0; i < obstaculos.length; i++) {
+        const obstaculo = obstaculos[i];
+        obstaculo.actualizar();
+        obstaculo.dibujar();
+        
+        let tamanoArbol = tamanosArboles[i];
+
+        contexto.drawImage(imagenArbol, obstaculo.x + tamanoArbol, alturaCanvas - tamanoArbol - alturaPiso, tamanoArbol, tamanoArbol);
+
+        if (personaje.chocaCon(obstaculo) && estadoDelJuego == Estado.INICIADO) {
+            estadoDelJuego = Estado.TERMINADO;
+            choque.play();
+            if (puntuacion > puntuacionMasAlta) {
+                puntuacionMasAlta = puntuacion;
+            }
+
+            contexto.font = '100px Arial';
+            contexto.fillText('Perdido', anchura / 2 - 150, alturaCanvas / 2);
+            contexto.font = '30px Arial';
+            contexto.fillText('Doble click para continuar', anchura / 2 - 150, alturaCanvas / 2 + 50);
+        }
+    }
+}
 
 function inicializarJuego() {
     obstaculos = [];
     tamanosArboles = [];
+    personaje.inicializar();
 
     for (let i = 0, x = anchura; i < totalDeObstaculos; i++, x += Math.round(Math.random() * 400) + 600) {
         let alturaObstaculo = Math.floor(Math.random() * 30) + 100;
@@ -116,12 +223,13 @@ function inicializarJuego() {
 
         tamanosArboles.push(Math.floor(Math.random() * 200) + 100);
     }
+    estadoDelJuego = Estado.ANTES_DE_EMPEZAR;
 }
 
 function crearObstaculo(x, alturaObstaculo, posicionObstaculo) {
     return {
         x: x, y: alturaCanvas - alturaObstaculo, anchura: 70, altura: alturaObstaculo,
-        dibujar(contexto) {
+        dibujar() {
             contexto.fillStyle = "rgb(150, 0, 0)";
             contexto.beginPath();
             contexto.fillRect(this.x, this.y, this.anchura, this.altura);
@@ -130,6 +238,11 @@ function crearObstaculo(x, alturaObstaculo, posicionObstaculo) {
             contexto.beginPath();
             contexto.fillText(posicionObstaculo, this.x + 10, this.y);
             contexto.fill();
+        },
+        actualizar() {
+            if( estadoDelJuego == Estado.INICIADO ) {
+                this.x -= variacionHorizontal;
+            }
         }
     };
 }
@@ -138,34 +251,12 @@ function gradosARadianes(grados) {
     return grados * (Math.PI / 180);
 }
 
-//1 - Dibujar Cuadricula
-function dibujarCuadricula(variacion = 20) {
-    for (let x = variacion; x < anchura; x += variacion) {
-        crearLinea({ x, y: 0 }, { x, y: alturaCanvas }).dibujar();
-    }
-
-    for (let y = variacion; y < alturaCanvas; y += variacion) {
-        crearLinea({ x: 0, y }, { x: anchura, y }).dibujar();
-    }
-}
-
-//4- Dibujar una imagen
 
 let imagenActual = 0,
     anchuraImagenInicial,
     alturaImagenInicial,
     estaCreciendo = false,
     contidadASaltar = 0;
-
-// personaje.imagen = new Image();//document.getElementById(`gato${imagenActual}`);
-
-// personaje.imagen.src = "imagenes/" + personaje.nombre + "/" + personajes.gato[0];
-// console.log( personaje.imagen );
-
-// personaje.anchura = personaje.imagen.width;
-// personaje.altura = personaje.imagen.height;
-
-//personaje.imagen.onload = iniciar;
 
 let arregloImagenes = [];
 let rutaAImagenesDelPersonaje = personajes[personaje.nombre];
@@ -187,7 +278,7 @@ function cargarImagenes(){
     arregloImagenes = [];
     for( let i = 0; i < rutaAImagenesDelPersonaje.length; i++) {
         nuevaImagen = new Image();
-        arregloImagenes.push( nuevaImagen );//document.getElementById(`gato${imagenActual}`);
+        arregloImagenes.push( nuevaImagen );
         let rutaImagenActual = "imagenes/" + personaje.nombre + "/" + rutaAImagenesDelPersonaje[i];
 
         nuevaImagen.src = rutaImagenActual;
@@ -216,7 +307,7 @@ function dibujarImagen() {
     alturaImagenInicial = alturaImagenInicial || personaje.imagen.height;
     personaje.anchura = anchuraImagenInicial / 3;
     personaje.altura = alturaImagenInicial / 3;
-    personaje.y = alturaCanvas - personaje.altura;
+    personaje.y = miCanvas.height - personaje.altura;
 
     let porcentajeAltura = anchuraImagenInicial / alturaImagenInicial;
 
@@ -234,18 +325,7 @@ function dibujarImagen() {
     }
 }
 
-function funcionALlamar() {
-
-    contexto.beginPath();
-    contexto.fillStyle = "#eee";
-    contexto.fillRect(0, 0, anchura, alturaCanvas);
-
-    let variacionHorizontal = 20;
-
-    if (PropCanvas.posicionHorizontal > anchura) {
-        PropCanvas.posicionHorizontal = -personaje.anchura;
-    }
-
+function dibujarPiso() {
     //dibujar el piso
     contexto.beginPath();
     contexto.fillStyle = "gray";
@@ -254,127 +334,52 @@ function funcionALlamar() {
     //dibujar lineas verticales para hacer mas realista el movimiento
     let variacion = personaje.anchura;
     
-    for (let x = anchura - xInicial; x > -variacion; x -= variacion) {
-        //  contexto.beginPath();
-        //  contexto.strokeStyle = "black";
-        //  contexto.moveTo(x, 0);
-        //  contexto.lineTo(x, altura - 20);
-        //  contexto.stroke();
-    
-        //  contexto.beginPath();
-        //  contexto.strokeStyle = "white";
-        //  contexto.moveTo(x, altura - 20);
-        //  contexto.lineTo(x + 40, altura);
-        //  contexto.stroke();
-    
+    for (let x = anchura - xInicial; x > -variacion; x -= variacion) {    
          contexto.beginPath();
          contexto.strokeStyle = "white";
          contexto.moveTo(x + variacion / 2, alturaCanvas - 20);
          contexto.lineTo(x + variacion / 2 + 40, alturaCanvas);
          contexto.stroke();
      }
+
+     if(estadoDelJuego == Estado.INICIADO) {
+        xInicial += variacionHorizontal;
+        if (xInicial > variacion) {
+            xInicial = 0;
+        }
+    }
+ 
+}
+
+function funcionALlamar() {
+
+    contexto.beginPath();
+    contexto.fillStyle = "#eee";
+    contexto.fillRect(0, 0, anchura, alturaCanvas);
+
     
-    xInicial += variacionHorizontal;
-    if (xInicial > variacion) {
-        xInicial = 0;
+
+    if (PropCanvas.posicionHorizontal > anchura) {
+        PropCanvas.posicionHorizontal = -personaje.anchura;
     }
 
+    dibujarPiso();
+    
     let posicionVertical = alturaCanvas - personaje.altura;
 
     contexto.strokeStyle = "black";
 
-    //dibujar lineas linea horizontales desde encima de la imagen para hacer aun mas realista el movimiento
-    /*
-    for (let y = posicionVertical; y > 0; y -= altura - posicionVertical) {
-        contexto.beginPath();
-        contexto.moveTo(0, y);
-        contexto.lineTo(anchura, y);
-        contexto.stroke();
+    if(estadoDelJuego == Estado.INICIADO ) {
+        puntuacion += 1;
     }
-    */
-
-    let arregloPersonajeActual = personajes[ personaje.nombre ];
-
-    if (estaSaltando) {
-        imagenActual = estaSubiendo ? arregloPersonajeActual.length - 2 : arregloPersonajeActual.length - 1;
-    }
-    else {
-        imagenActual += 2
-        if (imagenActual >= personajes[ personaje.nombre ].length) {
-            imagenActual = 0;
-        }
-    }
-
-    //dibujar la imagen del gato
-
-    puntuacion += 1;
     contexto.fillStyle = "black";
     contexto.font = '30px Arial';
     contexto.fillText('Puntuacion: ' + puntuacion, 100, 50);
     contexto.fillText('Puntuacion Mas Alta: ' + puntuacionMasAlta, 500, 50);
-
-    if (estaSaltando) {
-
-        cantidadASaltar += (estaSubiendo ? 60 : -60);
-
-        if (cantidadASaltar > maximoASaltar) {
-            cantidadASaltar = maximoASaltar;
-            estaSubiendo = false;
-        }
-
-        if (estaSubiendo) {
-            personaje.y -= 20;
-
-        } else {
-
-            personaje.y += 20;
-            if (personaje.y > alturaCanvas - personaje.altura) {
-                personaje.y = alturaCanvas - personaje.altura;
-            }
-            if (cantidadASaltar <= 0) {
-                estaSaltando = false;
-                contidadASaltar = 0;
-            }
-        }
-
-    }
-
-
-
-    for (let i = 0; i < obstaculos.length; i++) {
-        const obstaculo = obstaculos[i];
-        obstaculo.dibujar(contexto);
-        obstaculo.x -= variacionHorizontal;
-        let tamanoArbol = tamanosArboles[i];
-
-        contexto.drawImage(imagenArbol, obstaculo.x + tamanoArbol, alturaCanvas - tamanoArbol - alturaPiso, tamanoArbol, tamanoArbol);
-
-        if (personaje.chocaCon(obstaculo)) {
-            PropCanvas.juegoPerdido = true;
-            estaSaltando = false;
-
-            contexto.font = '100px Arial';
-            contexto.fillText('Perdido', anchura / 2 - 150, alturaCanvas / 2);
-            contexto.font = '30px Arial';
-            contexto.fillText('Doble click para continuar', anchura / 2 - 150, alturaCanvas / 2 + 50);
-
-        }
-
-    }
-
-    personaje.imagen = arregloImagenes[imagenActual]; //document.getElementById(`gato${cantidadASaltar == maximoASaltar ? 2 : imagenActual}`);
-
-    contexto.drawImage(personaje.imagen, personaje.x, personaje.y, personaje.anchura, personaje.altura);
-
-    let hitBox = personaje.hitBox();
-    if ( hitBox ) {
-        contexto.strokeStyle = "red";
-        contexto.strokeRect(hitBox.x, hitBox.y, hitBox.anchura, hitBox.altura);
-
-    }
-
-    // contexto.strokeStyle = "blue";
-    // contexto.strokeRect(personaje.x, personaje.y, personaje.anchura, personaje.altura);
+    
+    dibujarObstaculos();
+    personaje.actualizar();
+    personaje.dibujar();
 
     contexto.strokeText(tiempo, 100, 100 );
 
@@ -391,38 +396,31 @@ function funcionALlamar() {
 
 
 miCanvas.addEventListener("click", () => {
-    console.log( PropCanvas );
 
-    if(PropCanvas.juegoPerdido) {
-        choque.play();
-        puntuacion = 0;
-        tiempo = 200;
-        clearInterval( PropCanvas.intervalo );
-        PropCanvas.intervalo = setInterval(funcionALlamar, tiempo);
-
-        if (puntuacion > puntuacionMasAlta) {
-            puntuacionMasAlta = puntuacion;
-        }
-
-        PropCanvas.juegoPerdido = false;
-    }
-    else if (PropCanvas.estaPausado) {
+    if( estadoDelJuego == Estado.ANTES_DE_EMPEZAR || estadoDelJuego == Estado.PAUSADO ) {
         inicio.play();
-        PropCanvas.estaPausado = false;
-        dibujarImagen();
+        estadoDelJuego = Estado.INICIADO;
+        personaje.inicializar();
     }
-    else {
-        pausa.play();
-        estaPausado = true;
-        clearInterval(PropCanvas.intervalo);
+    else if( estadoDelJuego == Estado.INICIADO) {
+        estadoDelJuego = Estado.PAUSADO;
     }
-    
+    else if(estadoDelJuego == Estado.TERMINADO) {
+        puntuacion = 0;
+        estadoDelJuego = Estado.ANTES_DE_EMPEZAR;
+        inicializarJuego();
+    } 
 });
 
 document.addEventListener("keypress", function () {
-    if (!estaSaltando) {
+    if( estadoDelJuego == Estado.ANTES_DE_EMPEZAR || estadoDelJuego == Estado.PAUSADO ) {
+        inicio.play();
+        estadoDelJuego = Estado.INICIADO;
+        personaje.inicializar();
+    }
+    else if (!personaje.estaSaltando) {
         salto.play();
-        estaSaltando = true;
-        estaSubiendo = true;
+        personaje.estaSaltando = true;
+        personaje.estaSubiendo = true;
     }
 });
